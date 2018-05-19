b87cbffb2876855b3126626f8b01a8d5bd794a34
jobayerccj <jobayerccj@gmail.com>
2018-05-19 10:07:29 +0600

    entry adding option added

--- app/Http/Controllers/CashflowController.php
+++ app/Http/Controllers/CashflowController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Cashflow;
+use Validator;
 
 class CashflowController extends Controller
 {
@@ -43,7 +44,30 @@ class CashflowController extends Controller
      */
     public function store(Request $request)
     {
-      
+      $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'amount' => 'required',
+            'flow_type' => 'required'
+        ]);
+
+        if($validator->fails()){
+            $data['success'] = false;
+            $data['message'] = $validator->errors();
+            echo json_encode($data);
+        }
+        else{
+            //add new cashbook entry
+            Cashflow::create([
+                    'name' => $request['name'],
+                    'amount' =>   $request['amount'],
+                    'flow_type' => $request['flow_type'],
+                    'description' => $request['description'],
+            ]);
+
+            $data['success'] = true;
+            $data['message'] = "New entry named <b>".$request['name']."</b> successfully added in cashbook";
+            echo json_encode($data);
+        }
     }
 
     /**
--- resources/views/cashbook/list.blade.php
+++ resources/views/cashbook/list.blade.php
@@ -17,14 +17,12 @@
             <div class="col-xs-12">
               <div class="box box-info">
                 <div class="box-header">
-                  <h3 class="box-title pull-right"><a href="{{ URL('/languages/create')}}">+ Add Cashflow</a></h3>
+                  <h3 class="box-title pull-right"><a href="#" data-toggle="modal" data-target="#addCashbookModal">+ Add Cashflow</a></h3>
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
-                  @if(Session::has('success_message'))
-                      <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('success_message') }}</p>
-                  @endif
-
+                  <div id="success_message1"></div>
+                 
                   @if (count($cashflow_list) > 0)
                   <table class="table table-bordered table-hover DataTable" id="cashflow_list">
                     <thead>
@@ -80,11 +78,61 @@
         </div>
     </div>
 
+    <!-- Modal for cashbook entry -->
+    <div class="modal fade" id="addCashbookModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="exampleModalLabel">Insert Cashbook</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form method="POST" id="cashbook_entry_form1">
+          <div class="modal-body">
+              <div id="validation_errors1">
+                
+              </div>
+              {{ csrf_field() }}
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Name</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="My first payment" name="name">
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlInput2">Amount</label>
+                <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="1234" name="amount">
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Flow Type</label>
+                <select class="form-control" id="exampleFormControlSelect1" name="flow_type">
+                  <option value="">...</option>
+                  <option value="1">Cash Inflow</option>
+                  <option value="2">Cash Outflow</option>
+                  
+                </select>
+              </div>
+              
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Description</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+              </div>
+            
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
     <script type="text/javascript">
 
        $(document).ready(function(){
            $('#cashflow_list').dataTable({
                responsive: true,
+               "aaSorting": [],
            });
        });
 
@@ -121,6 +169,36 @@
                 });
             });
           }
+
+          $(document).on('submit', "#cashbook_entry_form1", function(e){
+            
+            e.preventDefault();
+            
+            $.ajax({
+              type: "POST",
+              data: $(this).serialize(),
+              url: "./cashflow",
+              success: function(response){
+                var result = JSON.parse(response);
+                if(result['success']){
+                  $("#addCashbookModal").modal('hide');
+                  $("#success_message1").html('<div class="alert alert-success" role="alert">'+result['message'] +'</div>');
+                }
+                else{
+                  var error_list = "";
+                  $.each(result['message'], function(index, value){
+                    error_list += '<li class="list-group-item list-group-item-danger">' + value +'</li>';
+                  });
+
+                  $("#validation_errors1").html('<ul class="list-group">' + error_list +"</ul>");
+                }
+
+                console.log(result);
+              }
+            });
+          });
     </script>
   </div>
+
+  
 @endsection
